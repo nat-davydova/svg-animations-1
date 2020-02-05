eb7cfe2af217a89e3401ac9a9fde86faf5fc995b
nat-davydova <natti.davydova@gmail.com>
2020-02-05 18:49:40 +0300

    Icon 1: arrow animation

--- dist/script.js
+++ dist/script.js
@@ -8,17 +8,36 @@ const cartIconAnimation = anime({
     value: [anime.setDashoffset, 0],
     easing: 'easeInOutSine',
     duration: 1500,
-    direction: 'alternate' },
+    direction: 'alternate',
+    loop: false },
 
 
   fill: {
     value: '#000',
     easing: 'linear',
     duration: 400,
-    delay: 1500 },
+    delay: 1500,
+    loop: false },
 
 
-  loop: false });
+  complete: () => {
+
+    anime.set(`${cartIcon} .arrow`, {
+      translateY: 0 });
+
+
+    const arrowAnimation = anime({
+
+      targets: `${cartIcon} .arrow`,
+      translateY: -30,
+      duration: 1000,
+      easing: 'easeOutElastic',
+      loop: true });
+
+
+
+  } });
+
 
 
 //*** REPLAYS INIT
