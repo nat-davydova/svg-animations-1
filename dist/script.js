8158b03e650c6f6d04f605ddf07032f69ccdc35f
nat-davydova <natti.davydova@gmail.com>
2020-02-16 18:52:14 +0300

    Icon #2: applied basic structure

--- dist/script.js
+++ dist/script.js
@@ -2,48 +2,6 @@ const cartIcon = '.cart-icon';
 
 //*** ICONS ANIMATIONS
 
-// const cartIconAnimationFunc = async () => {
-
-//   const lineAnimation = anime({
-//     targets: `${cartIcon} path`,
-
-//     strokeDashoffset: {
-//       value: [anime.setDashoffset, 0],
-//       easing: 'easeInOutSine',
-//       duration: 1500,
-//       direction: 'alternate',
-//     }
-
-//   }).finished;
-
-//   const fillAnimation = anime({
-//     targets: `${cartIcon} path`,
-
-//     fill: {
-//       value: '#000',
-//       easing: 'linear',
-//       duration: 400,
-//       delay: 1500,
-//     },
-
-//   }).finished;
-
-//   await Promise.all([lineAnimation, fillAnimation]);
-
-// };
-
-// cartIconAnimationFunc()
-//   .then(() => {
-//     anime({
-//       targets: `${cartIcon} .arrow`,
-//       translateY: [-30, 0],
-//       duration: 700,
-//       easing: 'easeOutElastic',
-//       direction: 'alternate',
-//       loop: true
-//     });
-// });
-
 let cartBounceAnimation;
 
 const cartIconAnimation = anime({
